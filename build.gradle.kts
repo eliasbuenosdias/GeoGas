30ff7d7b50e8f116846e7913d987310ef9e9e3ae
Elías <19223547+eliasbuenosdias@users.noreply.github.com>
2026-01-02 15:18:21 +0100

    ...

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,12 +4,3 @@ plugins {
     //alias(libs.plugins.kotlin.android) apply false
     //alias(libs.plugins.kotlin.compose) apply false
 }
-
-android {
-    dependenciesInfo {
-        // Disables dependency metadata when building APKs (for IzzyOnDroid/F-Droid)
-        includeInApk = false
-        // Disables dependency metadata when building Android App Bundles (for Google Play)
-        includeInBundle = false
-    }
-}
