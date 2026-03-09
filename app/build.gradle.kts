1a9cbd7ef7a0dcce453d519c8117c4d3a999ac9b
Elías <19223547+eliasbuenosdias@users.noreply.github.com>
2026-03-09 18:11:05 +0100

    Fix: Permisos de release y ajuste a v1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,9 @@ android {
         applicationId = "com.eliasbuenosdias.geogas"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.1"
+
 
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
